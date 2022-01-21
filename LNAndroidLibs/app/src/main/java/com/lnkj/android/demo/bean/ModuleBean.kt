459f394d7a73e19b88c1fcfe06630ecfcc486c79
MuFeng <hanlin_bj@163.com>
2022-01-21 22:43:35 +0800

    保存视频和图片到相册

--- LNAndroidLibs/app/src/main/java/com/lnkj/android/demo/bean/ModuleBean.kt
+++ LNAndroidLibs/app/src/main/java/com/lnkj/android/demo/bean/ModuleBean.kt
@@ -2,6 +2,7 @@ package com.lnkj.android.demo.bean
 
 import android.app.Activity
 import com.lnkj.android.demo.ui.page.gallery.GallerySaveActivity
+import com.lnkj.epublibs.BookListActivity
 
 /**
  *
@@ -15,5 +16,12 @@ val mainDataList = arrayListOf(
         title = "保存图片视频到本地",
         content = "保存图片视频到本地相册， 兼容Android10+ 以及鸿蒙系统",
         clzz = GallerySaveActivity::class.java
+    ),
+
+    // 电子书
+    ModuleBean(
+        title = "电子书",
+        content = "epub电子书",
+        clzz = BookListActivity::class.java
     )
 )
